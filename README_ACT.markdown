947c4b0003911fdf56565cd837eef03ce09f6644
Alejandra Villa <dora.villa@est.iudigital.edu.co>
2022-12-11 20:09:44 -0500

    Entrega

---
+++ README_ACT.markdown
@@ -0,0 +1,9 @@
+#Actividad 2
+
+###Integrates del equipo
+
+- Dora Alejandra Villa Aguirre - PREELEC2202PC-TDS0033
+
+
+
+
---
+++ src/router/index.js
@@ -0,0 +1,59 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import HomePView from '../views/HomePostView.vue';
+import CreateView from '../views/CreateView.vue';
+import EditView from '../views/EditView.vue';
+import CreatePView from '../views/CreatePostView';
+import EditPView from '../views/EditPostView';
+import LoguinView from '../views/LoguinView';
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+
+  {
+    path: '/loguin',
+    name: 'loguin',
+    component: LoguinView
+  },
+
+  {
+    path: '/homep',
+    name: 'homep',
+    component: HomePView
+  },
+
+  {
+    path: '/create',
+    name: 'create',   
+    component: CreateView
+  },
+
+  {
+    path: '/edit/:id',
+    name: 'edit',   
+    component: EditView
+  },
+
+  {
+    path: '/createp',
+    name: 'createp',   
+    component: CreatePView
+  },
+
+  {
+    path: '/editp/:id',
+    name: 'editp',   
+    component: EditPView
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
